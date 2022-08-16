40fb00379226cb3d6547338052ad4e3a9698ee0e
Maria Agostina Albornoz <Agostinaalbornoz43@gmail.com>
2022-08-16 11:38:56 -0300

    modifique error de sintaxis

--- laPlataRes/src/main.jsx
+++ laPlataRes/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import "bootstrap/dist/css/boostrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import "animate.css"
 import './index.css'
