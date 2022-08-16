d8f7f6bef4573a9ca62016cd65b78d2021418a9e
Maria Agostina Albornoz <Agostinaalbornoz43@gmail.com>
2022-08-15 21:47:03 -0300

    login y registro en proceso

---
+++ laPlataRes/src/routes/RoutesDos.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import HomeScreen from "../pages/HomeScreen";
+
+
+const RoutesDos = () => {
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        
+      </Routes>
+    </>
+  );
+};
+
+export default RoutesDos;
